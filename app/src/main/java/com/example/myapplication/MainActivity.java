455f43c44dc9e32e418e21d2621e703339831bdb
salsabillaflsft <50274165+salsabillaflsft@users.noreply.github.com>
2021-06-24 03:40:40 +0700

    layout

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,14 +1,47 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
+
+    private BottomNavigationView navbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        navbar = findViewById(R.id.main_navbar);
+        navbar.setOnNavigationItemSelectedListener(this);
+        loadFragment(new Fragment());
+    }
+
+    private boolean loadFragment(Fragment fragment) {
+        if(fragment != null){
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.main_frame, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Fragment fragment = null;
+
+        switch(item.getItemId()){
+            case R.id.ic_home :
+                fragment = new HomeFragment();
+                break;
+        }
+        return loadFragment(fragment);
     }
 }
